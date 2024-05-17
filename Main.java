49589d0b23da3b9ce3765391c959312345dc13c7
LukaszKakiet <kakietlukasz@gmail.com>
2024-05-17 19:47:29 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
 import company.models.Worker;
 import company.models.Manager;
-
+import company.abstracts.Employee;
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Anna Kowalska", 4100.0, 1);
-        Worker worker2 = new Worker("Michał Nowak", 4500.0, 2);
-        Worker worker3 = new Worker("Katarzyna Wiśniewska", 4400.0, 3);
-        Worker worker4 = new Worker("Piotr Zieliński", 4400.0, 4);
-        Manager manager = new Manager("Joanna Kamińska", 7000.0, 5);
-        Worker[] workers = {worker1, worker2, worker3, worker4};
+        Worker worker1 = new Worker("Anna Kowalska", 4100.0, 1, "2022-08-14", "Worker");
+        Worker worker2 = new Worker("Michał Nowak", 4500.0, 2, "2021-12-05", "Worker");
+        Worker worker3 = new Worker("Katarzyna Wiśniewska", 4400.0, 3, "2023-04-28", "Worker");
+        Worker worker4 = new Worker("Piotr Zieliński", 4400.0, 4, "2022-11-20", "Worker");
+        Manager manager = new Manager("Joanna Kamińska", 7000.0, 5, "2021-10-03", "Manager");
+        Employee[] employees = { worker1, worker2, worker3, worker4, manager };
 
-        for (Worker worker : workers) {
-            System.out.println("Salary: " + worker.getSalary());
-            worker.work();
+        for (Employee employee : employees) {
+            employee.work();
+            System.out.println("-" + employee.getName() + " (ID: " + employee.getId() + ", Position: " +
+                    employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " +
+                    employee.getSalary() + ")");
         }
-        System.out.println("Salary: " + manager.getSalary());
-        manager.work();
     }
 }
--- company/models/Manager.java
+++ company/models/Manager.java
@@ -1,12 +1,16 @@
 package company.models;
 import company.abstracts.Employee;
 
-public class Manager extends Employee {
-    public Manager(String name, double salary, int id) {
-        super(name, salary, id);
+public final class Manager extends Employee {
+    public Manager(String name, double salary, int id, String hireDate, String position) {
+        super(name, salary, id, hireDate, position);
     }
     @Override
     public void work() {
         System.out.println("Manager " + getName() + " is managing...");
+    }
+    @Override
+    public String getName() {
+        return super.getName();
     }
 }
